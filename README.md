86c1f1f9930fbe45fce2fa786f31740b576c0ba4
Winggo Tse <winggo.tse@gmail.com>
2025-05-04 00:18:26 -0700

    initial: generate and store embeddings

---
+++ README.md
@@ -0,0 +1,66 @@
+# Memex
+
+A personal LLM leveraging my corpus as its knowledge base.
+
+Data sources:
+- Google
+    - Emails (mbox)
+    - Calendar (ics)
+    - Google Search Histroy (html, json)
+    - Youtube Search History (html)
+    - Contacts (vcf)
+    - Drive (docx)
+    - Maps (csv)
+- Apple
+    - Notes (txt)
+    - Contacts (vcf)
+    - Calendar (ics)
+
+
+## Implementation Steps
+
+1. Download data from sources Google Takeout & iCloud Exports
+2. Data processing: clean, chunk, and generating embeddings
+3. Store embeddings into a vector DB
+4. Leverage an open-source LLM for inference and to perform RAG
+5. Use iMessage as the LLM's user interface
+
+
+## Architecture
+
+1. Parsing
+    - langchain loaders + unstructured
+2. Chunking & Preprocessing
+    - langchain
+3. Embedding
+    - sentence-transformers (e.g., all-MiniLM-L6-v2)
+4. Vector DB
+    - FAISS
+5. Retriever & RAG Model
+    - mistral, llama, phi2 + langchain
+6. Optional UI
+    - Streamlit / Gradio
+
+
+## Attribution
+
+This project idea was inspired by this post from Linus: https://x.com/thesephist/status/1629272600156176386
+
+
+## Setup Issues
+
+1.
+Upon executing `python3 src/parsing.py`, if you encounter the following error:
+```
+[nltk_data] Error loading punkt_tab: <urlopen error [SSL:
+[nltk_data]     CERTIFICATE_VERIFY_FAILED] certificate verify failed:
+[nltk_data]     unable to get local issuer certificate (_ssl.c:1000)>
+```
+
+Fix: `sudo /Applications/Python\ 3.12/Install\ Certificates.command`
+
+
+2.
+`pdf2image.exceptions.PDFInfoNotInstalledError: Unable to get page count. Is poppler installed and in PATH?`
+
+Fix: `brew install poppler`
---
+++ src/generate_embeddings.py
@@ -0,0 +1,66 @@
+import os
+from dotenv import load_dotenv
+from glob import glob
+
+from unstructured_client import UnstructuredClient
+from langchain_unstructured import UnstructuredLoader
+from unstructured.cleaners.core import clean_extra_whitespace
+from langchain_together import TogetherEmbeddings
+from langchain_community.vectorstores import Chroma
+
+load_dotenv(".env")
+
+
+folder_path = "./data/apple/notes"
+file_paths = [
+    path for path in glob(os.path.join(folder_path, "**/*"), recursive=True)
+    if os.path.isfile(path)
+]
+
+if os.getenv("USE_UNSTRUCTURED_API") == "true":
+    loader = UnstructuredLoader(
+        file_paths,
+        post_processors=[clean_extra_whitespace],
+        client=UnstructuredClient(api_key_auth=os.getenv("UNSTRUCTURED_API_KEY")),
+        partition_via_api=True,
+        chunking_strategy="by_title",
+    )
+    print("Using Unstructured API")
+else:
+    loader = UnstructuredLoader(
+        file_paths,
+        post_processors=[clean_extra_whitespace],
+        chunking_strategy="by_title",
+    )
+    print("Using Unstructured locally")
+
+
+# Parsing, chunking, preprocessing
+documents = loader.load()
+processed_documents = []
+for doc in documents:
+    if len(doc.page_content.strip()) < 30:
+        continue
+    doc.metadata["languages"] = ", ".join(doc.metadata["languages"])
+    doc.metadata["type"] = "apple_note"
+    processed_documents.append(doc)
+
+print(f"{len(processed_documents)} documents loaded.")
+
+
+# Generate embeddings and store in vector DB
+embedding_function = TogetherEmbeddings(
+    model="BAAI/bge-large-en-v1.5",
+    api_key=os.getenv("TOGETHER_API_KEY")
+)
+
+
+CHROMA_PATH = os.path.join(os.getcwd(), "chroma_db")
+vectorstore = Chroma.from_documents(
+    processed_documents,
+    embedding=embedding_function,
+    persist_directory=CHROMA_PATH,
+)
+vectorstore.persist()
+
+print("Embeddings generated and stored in vector store")
