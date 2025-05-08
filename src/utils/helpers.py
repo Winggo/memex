bcccc155d952f02966c36848b8e2002005cf97f8
Winggo Tse <winggo.tse@gmail.com>
2025-05-08 08:25:44 -0700

    better name

--- src/utils/helpers.py
+++ src/utils/helpers.py
@@ -2,8 +2,8 @@ import os
 from datetime import datetime
 
 
-def get_date_from_timestamp(timestamp):
-    return datetime.fromtimestamp(timestamp).date().isoformat()
+def get_date_from_epoch(epoch):
+    return datetime.fromtimestamp(epoch).date().isoformat()
 
 
 def get_file_metadata(path):
@@ -15,9 +15,9 @@ def get_file_metadata(path):
             return mtdata
 
         if stats.st_birthtime:
-            mtdata["created_at"] = get_date_from_timestamp(stats.st_birthtime)
+            mtdata["created_at"] = get_date_from_epoch(stats.st_birthtime)
         if stats.st_mtime:
-            mtdata["updated_at"] = get_date_from_timestamp(stats.st_mtime)
+            mtdata["updated_at"] = get_date_from_epoch(stats.st_mtime)
 
         return mtdata
     except:
