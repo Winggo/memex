cb7d9473ab248cb278ae7e3af6ef9ebd426b81fa
Winggo Tse <winggo.tse@gmail.com>
2025-05-08 05:06:13 -0700

    don't generate multipart/mixed eml files
    cannot be processed by unstructuredLoader

--- src/scripts/process_mbox_file.py
+++ src/scripts/process_mbox_file.py
@@ -22,9 +22,14 @@ parser.add_argument(
 args = parser.parse_args()
 
 mbox = mailbox.mbox(args.file_path)
-if args.dry_run:
-    print(f"{len(mbox)} emails found")
-else:
-    for i, message in enumerate(mbox):
-        with open(os.path.join(args.output_dir, f"email_{i}.eml"), "w", encoding="utf-8") as f:
+print(f"{len(mbox)} emails found")
+
+if not args.dry_run:
+    email_id = 1
+    for message in mbox:
+        if message.get_content_type() == "multipart/mixed":
+            continue
+        
+        with open(os.path.join(args.output_dir, f"email_{email_id}.eml"), "w", encoding="utf-8") as f:
             f.write(message.as_string())
+        email_id += 1
