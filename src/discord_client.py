ba71dbe2aeb96a1722eabe328507948fe707052f
Winggo Tse <winggo.tse@gmail.com>
2025-05-16 01:13:51 -0700

    capitalize

--- src/discord_client.py
+++ src/discord_client.py
@@ -6,13 +6,13 @@ from src.utils.constants import DISCORD_APP_ID
 
 class DiscordClient(discord.Client):
     async def on_ready(self):
-        print(f"[Discord] logged on as {self.user}")
+        print(f"[Discord] Logged on as {self.user}")
 
     async def on_message(self, message):
         if self.application_id != DISCORD_APP_ID or message.author.bot:
             return
 
-        print(f"[Discord] message received from {message.author}")
+        print(f"[Discord] Message received from {message.author}")
         completion = respond_with_retrieved_context(message.content)
         await message.channel.send(completion)
 
