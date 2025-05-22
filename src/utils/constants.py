ac61e83f97c6b99976538a30cdcc038ed9cbd45e
Winggo Tse <winggo.tse@gmail.com>
2025-05-21 21:05:45 -0700

    set CHROMA_PATH in env

--- src/utils/constants.py
+++ src/utils/constants.py
@@ -1,5 +1,5 @@
 import os
 
-CHROMA_PATH = os.path.join(os.getcwd(), "chroma_db")
+CHROMA_PATH = os.path.join(os.getcwd(), os.environ.get("CHROMA_DIRNAME", "chroma_db"))
 DISCORD_APP_ID = 1369249100507123722
 MEMEX_MESSAGE_MARKER = "\u200B\u200C\u200D"
